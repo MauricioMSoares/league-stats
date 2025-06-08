66a05b9ebbd2f5a3ad14194086d3c91a4ef6852c
Mauricio Soares <mauricio.massayuki@gmail.com>
2025-06-08 19:22:13 -0300

    feat: added select component.

---
+++ app/components/select/index.py
@@ -0,0 +1,15 @@
+from shiny import ui, module
+
+
+@module.ui
+def select_ui(select_id: str, label: str, choices: dict[str]):
+    return ui.input_select(
+        id=select_id,
+        label=label,
+        choices=choices,
+    )
+
+
+@module.server
+def select_server(input, output, session):
+    pass
